e4de3a0ded0eecaa6b6dee7ef4f512955fe108df
vedant2863 <jvedant727@gmail.com>
2025-01-18 11:51:20 +0530

    leve setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+
+function App() {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero/>
+    </main>
+  );
+} 
+
+export default App;
